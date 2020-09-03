70ae592600b62def60c9c21afb5204372fbbbe7f
mshsky <mustafa.albayati@g.austincc.edu>
2020-09-02 21:58:05 -0500

    added for the next time

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+ 
+ * /*********************
+ *  Name: Mustafa Albayati
+ *  Coding 01
+ *Purpose: * This is a simple Hello World in C++
+ **********************/
+
+/* 
+ * File:   main.cpp
+ * Author: Alex Katrompas
+ */
+
+#include "main.h"
+
+int main(int argc, char** argv) {
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
